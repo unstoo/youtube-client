5d18a8d8cb4edb7c1e470134e82e7883a71e9ae7
unstoo <unstoo@gmail.com>
2021-08-25 16:48:07 +0300

    feat: generate components for the videos section

---
+++ src/app/video/video-grid/video-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-grid',
+  templateUrl: './video-grid.component.html',
+  styleUrls: ['./video-grid.component.scss']
+})
+export class VideoGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
