625f190c6dc41214158d65f31996b48846699b1c
unstoo <unstoo@gmail.com>
2021-08-25 16:51:47 +0300

    feat: add Video interface and mock videos data

---
+++ src/app/video.ts
@@ -0,0 +1,26 @@
+export interface Video {
+  snippet: {
+    publishedAt: string,
+    title: string,
+    thumbnails: {
+      default: Thumbnail,
+      medium: Thumbnail,
+      high: Thumbnail,
+      standard: Thumbnail,
+      maxres: Thumbnail,
+    },
+  },
+  statistics: {
+    viewCount: string,
+    likeCount: string,
+    dislikeCount: string,
+    favoriteCount: string,
+    commentCount: string,
+  },
+}
+
+interface Thumbnail {
+  url: string,
+  width: number,
+  height: number,
+};
