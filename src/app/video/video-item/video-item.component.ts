a6fb33de4f725157590827a94da707572ebbf5ec
unstoo <unstoo@gmail.com>
2021-08-26 01:06:02 +0300

    feat: add markup & logic for VideoItemComponent

--- src/app/video/video-item/video-item.component.ts
+++ src/app/video/video-item/video-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Video } from '../../video';
 
 @Component({
@@ -7,11 +7,47 @@ import { Video } from '../../video';
   styleUrls: ['./video-item.component.scss'],
 })
 export class VideoItemComponent implements OnInit {
-  video?: Video;
+  @Input() video?: Video;
+
+  date?: number;
+
+  currentClasses: any = {
+    ['video-item']: true,
+    underSevenDays: false,
+    underOneMonth: false,
+    underSixMonths: false,
+    overSixMonths: false,
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.video) {
+      this.setCurrentClasses(this.video.snippet.publishedAt);
+    }
+  }
+
+  setCurrentClasses(publishedAt: string): void {
+    const now = (new Date()).getTime();
+    const videoTime = (new Date(publishedAt)).getTime();
+    const diff = now - videoTime;
+    const day = 24 * 60 * 60 * 1000;
+    if (diff / day < 7) {
+      this.currentClasses.underSevenDays = true;
+      return;
+    }
+
+    if (diff / day < 31) {
+      this.currentClasses.underOneMonth = true;
+      return;
+    }
+
+    if (diff / day < 30 * 6) {
+      this.currentClasses.underSixMonths = true;
+      return;
+    }
+
+    this.currentClasses.overSixMonths = true;
   }
 
 }
