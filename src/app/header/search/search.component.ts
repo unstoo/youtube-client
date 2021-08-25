2664fb412df1de946d3b5f8dc7dfee68a6030439
unstoo <unstoo@gmail.com>
2021-08-25 21:55:53 +0300

    feat: add SearchComponent logic & markup

--- src/app/header/search/search.component.ts
+++ src/app/header/search/search.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface SI {
+  input: string
+}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  input: string = '';
+  visible: boolean = false;
+  name: string = 'Your Name'
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  searchHandler() {
+    // this.visible = true;
+  }
+
+  toggleSettings() {
+    this.visible = !this.visible;
+  }
 }
