9dddd14e5c33df26b44728b09f8ac5fd7de0f183
unstoo <unstoo@gmail.com>
2021-08-26 17:33:57 +0300

    feat: extend template to use custom pipe & filter data from service

--- src/app/video/video-grid/video-grid.component.ts
+++ src/app/video/video-grid/video-grid.component.ts
@@ -1,7 +1,7 @@
+import { VideosService } from '../../videos.service';
 import { Component, OnInit } from '@angular/core';
 import { Video } from '../../video';
-import { VideosService } from '../../videos.service';
-
+import { VideoFilter } from '../../video-filter';
 
 @Component({
   selector: 'app-video-grid',
@@ -11,10 +11,13 @@ import { VideosService } from '../../videos.service';
 export class VideoGridComponent implements OnInit {
   videos: Video[] = [];
 
+  filter: VideoFilter =  { str: '' };
+
   constructor(private videosService: VideosService) { }
 
   ngOnInit(): void {
     this.videos = this.videosService.getVideos();
+    this.filter = this.videosService.getFilter();
   }
 
 }
