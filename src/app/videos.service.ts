9c67edfd9d469b43f191985544eb8428e7434bdd
unstoo <unstoo@gmail.com>
2021-08-26 16:02:33 +0300

    feat: add VideosService

---
+++ src/app/videos.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+// import { Observable, of  } from 'rxjs';
+import { Video } from './video';
+import { VIDEOS } from './mock-videos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VideosService {
+  videos: Video[] = VIDEOS;
+  constructor() { }
+
+  getVideos(): Video[] {
+    return this.videos;
+  }
+
+  sortByViewsAsc(): void {
+    const result = this.videos?.sort((videoA, videoB) => {
+      const a = +videoA.statistics.viewCount;
+      const b = +videoB.statistics.viewCount;
+      return  a - b;
+    });
+
+    this.videos = result;
+  }
+
+  sortByViewsDesc(): void {
+    const result = this.videos.sort((videoA, videoB) => {
+      const a = +videoA.statistics.viewCount;
+      const b = +videoB.statistics.viewCount;
+      return  b - a;
+    });
+
+    this.videos = result;
+  }
+
+  sortByDateAsc(): void {
+    const result = this.videos.sort((videoA, videoB) => {
+      const a = new Date(videoA.snippet.publishedAt).getTime();
+      const b = new Date(videoB.snippet.publishedAt).getTime();
+      return  a - b;
+    });
+
+    this.videos = result;
+  }
+
+  sortByDateDesc(): void {
+    const result = this.videos.sort((videoA, videoB) => {
+      const a = new Date(videoA.snippet.publishedAt).getTime();
+      const b = new Date(videoB.snippet.publishedAt).getTime();
+      return  b - a;
+    });
+
+    this.videos = result;
+  }
+
+
+}
