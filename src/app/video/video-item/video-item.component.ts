03c66186f6eca647ae4d6e35234788357f951f7f
unstoo <unstoo@gmail.com>
2021-08-29 15:32:41 +0300

    refactor: remove date-style logic from VideoItemComponent

--- src/app/video/video-item/video-item.component.ts
+++ src/app/video/video-item/video-item.component.ts
@@ -9,45 +9,9 @@ import { Video } from '../../video';
 export class VideoItemComponent implements OnInit {
   @Input() video?: Video;
 
-  date?: number;
-
-  currentClasses: any = {
-    ['video-item']: true,
-    underSevenDays: false,
-    underOneMonth: false,
-    underSixMonths: false,
-    overSixMonths: false,
-  };
-
   constructor() { }
 
   ngOnInit(): void {
-    if (this.video) {
-      this.setCurrentClasses(this.video.snippet.publishedAt);
-    }
-  }
-
-  setCurrentClasses(publishedAt: string): void {
-    const now = (new Date()).getTime();
-    const videoTime = (new Date(publishedAt)).getTime();
-    const diff = now - videoTime;
-    const day = 24 * 60 * 60 * 1000;
-    if (diff / day < 7) {
-      this.currentClasses.underSevenDays = true;
-      return;
-    }
-
-    if (diff / day < 31) {
-      this.currentClasses.underOneMonth = true;
-      return;
-    }
-
-    if (diff / day < 30 * 6) {
-      this.currentClasses.underSixMonths = true;
-      return;
-    }
-
-    this.currentClasses.overSixMonths = true;
   }
 
 }
