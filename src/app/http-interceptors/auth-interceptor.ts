fe40f63bea2b2c38f3aa20786808d80e22c1fbe4
unstoo <unstoo@gmail.com>
2021-09-14 11:45:06 +0300

    feat: add AuthService check to HttpInterceptor

--- src/app/http-interceptors/auth-interceptor.ts
+++ src/app/http-interceptors/auth-interceptor.ts
@@ -7,12 +7,20 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 
+import { AuthService } from '../auth/auth.service';
 import { KEY } from '../const/YT_KEY';
 import { YT_BASE_URL } from '../const/YT_BASE_URL';
+import { NEVER } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!this.authService.isAuth()) {
+      this.authService.logout();
+      return NEVER;
+    }
     const withBaseUrl = req.clone({ url: YT_BASE_URL + req.url });
     const withKey = withBaseUrl.clone({ setParams: { key: KEY } });
 
