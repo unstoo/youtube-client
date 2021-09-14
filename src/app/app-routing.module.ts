33dd88576f461288dc14737c0c9cb70d3e117089
unstoo <unstoo@gmail.com>
2021-09-14 14:21:57 +0300

    feat: add lazy AdminModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
   { path: '', redirectTo: '/video', pathMatch: 'full' },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    canLoad: [AuthGuard],
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
